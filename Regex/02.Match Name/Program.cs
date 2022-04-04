adfa5556164faf28665999d68af29c668fa38d6c
Viktor Kachamakov <Kachamakov@abv.bg>
2022-04-04 12:30:51 +0300

    Added Solution!

---
+++ Regex/02.Match Name/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace MatchName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var patern = @"\+359(-| )2\1[0-9]{3,}\1[0-9]{4,4}\b";
+            var text = Console.ReadLine();
+
+            MatchCollection regex = Regex.Matches(text, patern);
+
+
+            var last = regex.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", last));
+
+
+        }
+    }
+}
