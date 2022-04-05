f6161fe9013255bae88246e1413e363333a0a47d
Viktor <Kachamakov@abv.bg>
2022-04-06 01:08:41 +0300

    Added Solution

---
+++ Regex/03.Fancy Barcode/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace FancyBarcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"@{1}#+[A-Z]{1}[A-Za-z0-9]{4,}[A-Z]{1}@{1}#+";
+            var pattern1 = "[0-9]";
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine();
+
+                var text = Regex.Matches(line, pattern);
+                var text1 = Regex.Matches(line, pattern1);
+                var word = string.Empty;
+                var sum = string.Empty;
+                foreach (Match item in text)
+                {
+                    word = item.Value;
+
+                }
+                if (line != word)
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+                else
+                {
+                    foreach (Match item in text1)
+                    {
+                        sum += item.Value.ToString();
+                    }
+                    if (string.IsNullOrEmpty(sum))
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {sum}");
+                    }
+                }
+
+
+            }
+
+        }
+    }
+}
