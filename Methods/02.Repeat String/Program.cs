edca3b489a0098d03c327bd6b84e4b8f6941fa69
Viktor <Kachamakov@abv.bg>
2022-04-22 01:30:03 +0300

    Solution

---
+++ Methods/02.Repeat String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someString = Console.ReadLine();
+            var numToRepeat = int.Parse(Console.ReadLine());
+            repeatString(someString, numToRepeat);
+
+        }
+        static void repeatString(string someString, int numToRepeat)
+        {
+            var a = string.Empty;
+            for (int i = 0; i < numToRepeat; i++)
+            {
+                a += someString;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
