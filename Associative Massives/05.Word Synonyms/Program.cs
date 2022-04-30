a71b804d45d9a6da8186af56e4613cc9d0d143b9
Viktor Kachamakov <Kachamakov@abv.bg>
2022-04-30 14:40:58 +0300

    Solution!

---
+++ Associative Massives/05.Word Synonyms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var num = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < num; i++)
+            {
+                var word = Console.ReadLine();
+                var sinonim = Console.ReadLine();
+                if (!dict.ContainsKey(word))
+                {
+                    dict[word] = new List<string>();
+                }
+                dict[word].Add(sinonim);
+
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+
+        }
+    }
+}
+
