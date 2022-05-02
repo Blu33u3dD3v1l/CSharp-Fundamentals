0155eb52ad143e760adc17594ddcb3e25b4e77e3
Viktor <Kachamakov@abv.bg>
2022-05-02 23:32:15 +0300

    Added Homework

---
+++ Lists/02.List Operation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "end")
+                {
+                    break;
+                }
+                var tokens = line.Split();
+
+                if (tokens[0] == "Insert")
+                {
+                    var num = int.Parse(tokens[1]);
+                    var num1 = int.Parse(tokens[2]);
+                    list.Insert(num1, num);
+                }
+                if (tokens[0] == "Delete")
+                {
+                    var num = int.Parse(tokens[1]);
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (num == list[i])
+                        {
+                            list.Remove(list[i]);
+                        }
+                    }
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
+
