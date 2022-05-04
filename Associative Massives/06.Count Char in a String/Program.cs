30eabcc7e5fd4487ef0097525b042e648c6d392f
Viktor Kachamakov <Kachamakov@abv.bg>
2022-05-04 12:44:31 +0300

    Homework

---
+++ Associative Massives/06.Count Char in a String/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Char_in_a_Sring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            var dict = new Dictionary<char, int>();
+            {
+                foreach (var item in list)
+                {
+                    for (int i = 0; i < item.Length; i++)
+
+                        if (!dict.ContainsKey(item[i]))
+                        {
+                            dict[item[i]] = 1;
+                        }
+                        else
+                        {
+                            dict[item[i]]++;
+                        }
+
+
+
+                }
+                foreach (var kvp in dict)
+                {
+                    Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+                }
+            }
+        }
+    }
+}
