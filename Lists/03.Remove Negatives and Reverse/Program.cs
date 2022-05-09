e17955c993e59a3d7be423aed9949a7b74da6e0b
Viktor <Kachamakov@abv.bg>
2022-05-09 23:44:42 +0300

    solution

---
+++ Lists/03.Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            for (int i = 0; i < list.Count; i++)
+            {
+                list.RemoveAll(n => n < 0);
+            }
+            if (list.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+
+            list.Reverse();
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
