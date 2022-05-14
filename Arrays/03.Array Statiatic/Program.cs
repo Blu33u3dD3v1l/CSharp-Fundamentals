890873c561cdb080e9c1cb085b909767266adc3b
Viktor Kachamakov <Kachamakov@abv.bg>
2022-05-14 17:30:27 +0300

    Homework

---
+++ Arrays/03.Array Statiatic/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace array_statistic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split().ToArray();
+            while (true)
+            {
+                var text = Console.ReadLine();
+                if(text == "END")
+                {
+                    break;
+                }
+                var tokens = text.Split();
+                if(tokens[0] != "Reverse" && tokens[0] != "Replace" && tokens[0] != "Distinct")
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                if (tokens[0] == "Reverse")
+                {
+
+                    Array.Reverse(line);
+
+                }
+                if (tokens[0] == "Distinct")
+                {
+                    line = line.Distinct().ToArray();
+
+                }
+                if (tokens[0] == "Replace")
+                {
+                    var index = int.Parse(tokens[1]);
+                    var word = tokens[2];
+                    if (index <= line.Length - 1 && index >= 0)
+                    {
+                        line[index] = word;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                }
+               
+            }
+ 
+            Console.WriteLine(string.Join(", ", line));
+
+        }
+    }
+}
