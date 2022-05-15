46523f5636f71b181d4d925af7011f43613b55ca
Viktor Kachamakov <Kachamakov@abv.bg>
2022-05-15 15:03:23 +0300

    This is my homework

---
+++ Associative Massives/08.Phonebook/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Phonebook
+{
+    class Phonebook
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, string>();
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "END")
+                {
+                    break;
+                }
+                var tokens = line.Split();
+                if (tokens[0] == "A")
+                {
+                    var name = tokens[1];
+
+                    var phoneNumber = tokens[2];
+                    if (!dict.ContainsKey(name))
+                    {
+                        dict.Add(name, phoneNumber);
+                    }
+                    else
+                    {
+                        dict[name] = phoneNumber;
+                    }
+                }
+                if (tokens[0] == "S")
+                {
+                    if (dict.ContainsKey(tokens[1]))
+                    {
+
+                        Console.WriteLine($"{tokens[1]} -> {dict[tokens[1]]}");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {tokens[1]} does not exist.");
+
+                    }
+
+                }
+            }
+        }
+    }
+}
+
